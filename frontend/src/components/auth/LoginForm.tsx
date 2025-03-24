e9e5bd5dee73d7f5ee647bc3cc73ffe369d88fb7
shuji19960425 <121063604+shuji19960425@users.noreply.github.com>
2025-03-24 20:47:50 +0900

    middlewareの追加

--- frontend/src/components/auth/LoginForm.tsx
+++ frontend/src/components/auth/LoginForm.tsx
@@ -3,7 +3,7 @@
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { login } from "@/lib/api/auth";
 import { LoginFormSchema, loginSchema } from "@/lib/schema/loginSchema";
 
