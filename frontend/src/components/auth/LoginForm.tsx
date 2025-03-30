91289941f07e5b142c24702f892868975809e0ad
shuji19960425 <121063604+shuji19960425@users.noreply.github.com>
2025-03-30 15:25:39 +0900

    ログインページの修正

--- frontend/src/components/auth/LoginForm.tsx
+++ frontend/src/components/auth/LoginForm.tsx
@@ -45,10 +45,7 @@ export const LoginForm = () => {
   };
 
   return (
-    <form
-      onSubmit={handleSubmit(onSubmit)}
-      className="bg-white shadow-md rounded px-8 pt-6 pb-8 w-full max-w-sm"
-    >
+    <form onSubmit={handleSubmit(onSubmit)}>
       <h1 className="text-2xl font-bold mb-6 text-center">管理者ログイン</h1>
 
       {/* メールアドレス入力 */}
