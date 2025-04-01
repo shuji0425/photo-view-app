b34fb733d18f454aa2b1e152611c15276e935de5
shuji19960425 <121063604+shuji19960425@users.noreply.github.com>
2025-04-01 13:35:30 +0900

    ルート追加

--- backend/internal/router/photo_router.go
+++ backend/internal/router/photo_router.go
@@ -14,6 +14,7 @@ func SetupPhotoRoutes(r *gin.Engine, photoHandler *handler.PhotoHandler) {
 
 	// ルート
 	photoGroup.POST("/upload/:user_id", photoHandler.UploadPhotos)
+	photoGroup.POST("/bulk-update", photoHandler.BulkUpdatePhotos)
 	photoGroup.GET("/ids", photoHandler.GetPhotosByIDs)
 	photoGroup.GET("/", photoHandler.GetPaginatedPhotos)
 	photoGroup.DELETE("/", photoHandler.DeletePhotosByIDs)
