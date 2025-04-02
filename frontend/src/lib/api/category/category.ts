f2bfebfd05e1f650636f274a00530ab234c7c0d0
shuji19960425 <121063604+shuji19960425@users.noreply.github.com>
2025-04-02 11:42:58 +0900

    カテゴリーを共通化に修正

--- frontend/src/lib/api/category/category.ts
+++ frontend/src/lib/api/category/category.ts
@@ -1,23 +1,12 @@
-import { convertCategoryArray } from "@/lib/converters/category";
 import { Category } from "@/types/category";
-import { CategoryDTO } from "@/types/dto/category";
+import { apiFetch } from "../client";
 
 /**
  * カテゴリーを取得し変換して返却
  * @returns json
  */
-export const getCategories = async (): Promise<Category[]> => {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/admin/categories`,
-    {
-      credentials: "include",
-    }
+export const getCategories = (): Promise<Category[]> => {
+  return apiFetch<Category[]>(
+    `${process.env.NEXT_PUBLIC_API_URL}/admin/categories`
   );
-
-  if (!res.ok) {
-    throw new Error("カテゴリの取得に失敗しました");
-  }
-
-  const json: CategoryDTO[] = await res.json();
-  return convertCategoryArray(json);
 };
