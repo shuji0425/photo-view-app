83981266649b7524b39e8a47c839e9cad734b630
shuji19960425 <121063604+shuji19960425@users.noreply.github.com>
2025-04-03 20:21:16 +0900

    テキストエリアは改行によって範囲が変わるように修正

--- frontend/src/components/ui/Textarea.tsx
+++ frontend/src/components/ui/Textarea.tsx
@@ -1,18 +1,42 @@
+import { useCombinedRef } from "@/hooks/useCombinedRef";
 import { inputBaseClass } from "@/lib/styles/input";
 import { cn } from "@/lib/utils/cn";
-import { TextareaHTMLAttributes, forwardRef } from "react";
+import React, {
+  TextareaHTMLAttributes,
+  forwardRef,
+  useRef,
+  useEffect,
+} from "react";
 
 type Props = TextareaHTMLAttributes<HTMLTextAreaElement>;
 
 /**
- * テキストエリア
+ * テキストエリア（自動高さ調整）
  */
 export const Textarea = forwardRef<HTMLTextAreaElement, Props>(
   function TextareaBase({ className, ...props }, ref) {
+    const innerRef = useRef<HTMLTextAreaElement | null>(null);
+    const combinedRef = useCombinedRef(ref, innerRef);
+
+    // テキストエリアの高さを入力に応じて自動調整
+    const resizeHeight = () => {
+      const textarea = innerRef.current;
+      if (textarea) {
+        textarea.style.height = "auto";
+        textarea.style.height = textarea.scrollHeight + "px";
+      }
+    };
+
+    // 初期状態
+    useEffect(() => {
+      resizeHeight();
+    }, []);
+
     return (
       <textarea
-        ref={ref}
-        className={cn(inputBaseClass, className)}
+        ref={combinedRef}
+        className={cn(inputBaseClass, "resize-none overflow-hidden", className)}
+        onInput={resizeHeight}
         {...props}
       />
     );
