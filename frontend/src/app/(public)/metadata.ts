892b0eb2a9844e91a2bd7fce918f7ad6e078f83c
shuji19960425 <121063604+shuji19960425@users.noreply.github.com>
2025-04-13 14:58:01 +0900

    静的ページにもメタデータ追加

--- frontend/src/app/(public)/metadata.ts
+++ frontend/src/app/(public)/metadata.ts
@@ -3,11 +3,9 @@ import { createGenericMetadata } from "@/lib/utils/seo";
 /**
  * ホーム用のmetadata
  */
-export const homeMetadata = () => {
-  return createGenericMetadata({
-    title: "ホーム",
-    description:
-      "趣味のカメラとプログラミングを掛け合わせたサイトです。タグでの絞り込みや、ダブルタップで詳細情報の表示などができます。楽しんでいってください。",
-    pathname: "/",
-  });
-};
+export const homeMetadata = createGenericMetadata({
+  title: "ホーム",
+  description:
+    "趣味のカメラとプログラミングを掛け合わせたサイトです。タグでの絞り込みや、ダブルタップで詳細情報の表示などができます。楽しんでいってください。",
+  pathname: "/",
+});
---
+++ frontend/src/app/(public)/privacy/metadata.ts
@@ -0,0 +1,10 @@
+import { createGenericMetadata } from "@/lib/utils/seo";
+
+/**
+ * プライバシーポリシー用のmetadata
+ */
+export const privacyMetadata = createGenericMetadata({
+  title: "プライバシーポリシー",
+  description: "サイトのプライバシーポリシーに関する方針を記載しています。",
+  pathname: "/privacy",
+});
