f238fb9e14bf60bec82e86494fa783b9c19f00d2
shuji19960425 <121063604+shuji19960425@users.noreply.github.com>
2025-04-13 16:00:49 +0900

    url修正

--- frontend/src/lib/constants/metadata.ts
+++ frontend/src/lib/constants/metadata.ts
@@ -1,4 +1,5 @@
 // サイトの共通定義
 export const SITE_NAME = "Enframe";
-export const SITE_URL = "https://photo.shuji1996.com";
+export const SITE_URL =
+  process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
 export const DEFAULT_OGP_IMAGE = `${SITE_URL}/ogp.jpg`;
